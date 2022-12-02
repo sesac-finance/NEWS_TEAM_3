3a6c31a17e8762f9f7509b5292240eb9a7f1661e
Lee-Seulki <seulzzang1227@gmail.com>
2022-12-02 10:26:41 +0900

    Modify Code

--- daum_crawler/StickersCrawl.py
+++ daum_crawler/StickersCrawl.py
@@ -5,7 +5,7 @@ def stickers_crawl(itemKey: str):
     '''
     스티커 크롤링 함수
     '''
-    headers = {"authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb3J1bV9rZXkiOiJuZXdzIiwidXNlcl92aWV3Ijp7ImlkIjoxODQwNjExNSwiaWNvbiI6Imh0dHBzOi8vdDEuZGF1bWNkbi5uZXQvcHJvZmlsZS9jSFVGa2FKX2FaMTAiLCJwcm92aWRlcklkIjoiREFVTSIsImRpc3BsYXlOYW1lIjoi7J207Iqs6riwIn0sImdyYW50X3R5cGUiOiJhbGV4X2NyZWRlbnRpYWxzIiwic2NvcGUiOltdLCJleHAiOjE2Njk5MDE3NjgsImF1dGhvcml0aWVzIjpbIlJPTEVfREFVTSIsIlJPTEVfSURFTlRJRklFRCIsIlJPTEVfVVNFUiJdLCJqdGkiOiI3OGU2ZmExYi0zOGVkLTQ2MzUtYWYxNS05OTAyNTVjZTU3NjIiLCJmb3J1bV9pZCI6LTk5LCJjbGllbnRfaWQiOiIyNkJYQXZLbnk1V0Y1WjA5bHI1azc3WTgifQ.ZzQ_XtBLHh5XQK31OGRCBC5R31eJH-VUR538sIC0Ofc"}
+    headers = {"authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb3J1bV9rZXkiOiJuZXdzIiwiZ3JhbnRfdHlwZSI6ImFsZXhfY3JlZGVudGlhbHMiLCJzY29wZSI6W10sImV4cCI6MTY2OTk4Mzk0OCwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DTElFTlQiXSwianRpIjoiY2M0NmY3YzQtOTZiOC00MmExLWFmM2QtMmIxYjg5YWJhOTJiIiwiZm9ydW1faWQiOi05OSwiY2xpZW50X2lkIjoiMjZCWEF2S255NVdGNVowOWxyNWs3N1k4In0.Z8uOoHxjjVoM9mjp_YM_bn76k51HB9khO37tIxY3jPM"}
     reactionURL = 'https://action.daum.net/apis/v1/reactions/home?itemKey={}'.format(itemKey)
     response = requests.get(url = reactionURL, headers = headers)
     reactionjson = json.loads(response.text)['item']['stats']
